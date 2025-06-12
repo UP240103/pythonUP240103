581fc9a1a300137642ad00696c666e3fa7eef040
UP240103 <up240203@alumnos.upa.edu.mx>
2025-06-11 20:43:19 -0600

    Cambios para unir los codigos

--- Dia1/hello.py
+++ Dia1/hello.py
@@ -19,3 +19,34 @@ print(type('Mexico'))
 print(type('Im enjoying 30 days of python'))
 
 
+
+numero_entero = 42                
+numero_flotante = 3.14159         
+numero_complejo = 1 + 2j          
+
+cadena_ejemplo = "¡Hola, Python!"
+
+
+valor_booleano = True
+
+lista_ejemplo = [1, 2, 3, 4, 5]
+
+tupla_ejemplo = (10, 20, 30)
+
+conjunto_ejemplo = {1, 2, 3, 4}
+
+diccionario_ejemplo = {"nombre": "Alicia", "edad": 30, "ciudad": "Wonderland"}
+
+
+import math
+
+punto1 = (2, 3)
+punto2 = (10, 8)
+
+
+distancia = math.sqrt((punto2[0] - punto1[0]) ** 2 + (punto2[1] - punto1[1]) ** 2)
+
+print("La distancia euclidiana entre", punto1, "y", punto2, "es:", distancia)
+
+#cambios pra unir los dos programas
+
